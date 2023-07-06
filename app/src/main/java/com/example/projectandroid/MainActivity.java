a47311620b8255951330b93d6e09498cb55fabb4
Tuo238 <billbui3002@gmail.com>
2023-07-06 20:38:49 +0700

    Add Homepage

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageSlider imgSlider = findViewById(R.id.imgSlider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.american_food, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.chinese_food, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.thai_food, ScaleTypes.FIT));
+
+        imgSlider.setImageList(slideModels, ScaleTypes.FIT);
     }
 }
