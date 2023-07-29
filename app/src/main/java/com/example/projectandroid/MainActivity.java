fc7833138762d9ae541aa73a646f39daa59a651f
Tuo238 <billbui3002@gmail.com>
2023-07-29 13:51:12 +0700

    Add Nav

--- app/src/main/java/com/example/projectandroid/MainActivity.java
+++ app/src/main/java/com/example/projectandroid/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.projectandroid;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -12,6 +14,7 @@ import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -27,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     GridView gvMain;
     ArrayList<MainGridviewClass> mainGridviewClassArrayList;
     FirebaseFirestore db;
+
+    //Navigation
+    BottomNavigationView bottom_navigation;
+
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +61,37 @@ public class MainActivity extends AppCompatActivity {
         // here we are calling a method
         // to load data in our list view.
         loadDatainGridView();
+
+
+        //=========================
+        // Initialize and assign variable
+        bottom_navigation=findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottom_navigation.setSelectedItemId(R.id.home);
+
+        // Perform item selected listener
+        bottom_navigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.test) {
+                    startActivity(new Intent(getApplicationContext(), TestActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (itemId == R.id.home) {
+                    // Your code for handling the "test" item selected
+                    return true;
+                } else if (itemId == R.id.test2) {
+                    startActivity(new Intent(getApplicationContext(), Test2.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //==========================
     }
 
     private void loadDatainGridView() {
---
+++ app/src/main/java/com/example/projectandroid/TestActivity.java
@@ -0,0 +1,46 @@
+package com.example.projectandroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class TestActivity extends AppCompatActivity {
+    BottomNavigationView bottom_navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        // Initialize and assign variable
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        // Set Home selected
+        bottom_navigation.setSelectedItemId(R.id.home);
+
+        // Perform item selected listener
+        bottom_navigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (itemId == R.id.test) {
+                    // Your code for handling the "test" item selected
+                    return true;
+                } else if (itemId == R.id.test2) {
+                    startActivity(new Intent(getApplicationContext(), Test2.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
