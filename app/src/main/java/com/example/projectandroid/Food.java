3e29d443d32e436057cabf1508c2f0d9e71a2e6b
MiePhong <nguyenhoangkimlong274@gmail.com>
2023-08-07 23:46:48 +0700

    update

---
+++ app/src/main/java/com/example/projectandroid/Food.java
@@ -0,0 +1,57 @@
+package com.example.projectandroid;
+
+public class Food {
+        String img;
+        String name;
+        String mota;
+        String sale;
+        String id;
+
+    public Food(String img, String name, String mota, String sale, String id) {
+        this.img = img;
+        this.name = name;
+        this.mota = mota;
+        this.sale = sale;
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public String getSale() {
+        return sale;
+    }
+
+    public void setSale(String sale) {
+        this.sale = sale;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
