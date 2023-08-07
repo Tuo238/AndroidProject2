dfdaee73fe5637eb5844326f3ba3c528a1533a85
Trong <tronga7z6@gmail.com>
2023-08-07 20:23:48 +0700

    edit profile

--- app/src/main/java/com/example/projectandroid/Profile.java
+++ app/src/main/java/com/example/projectandroid/Profile.java
@@ -1,20 +1,29 @@
 package com.example.projectandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends AppCompatActivity {
 
     TextView Fname,Lname, Email, Logout,changeProfile;
+
+    ImageView images;
 
     FirebaseUser user;
 
@@ -23,6 +32,9 @@ public class Profile extends AppCompatActivity {
     ProgressBar progressBar;
 
     FirebaseAuth auth;
+
+    private boolean isActivityDestroyed = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +53,7 @@ public class Profile extends AppCompatActivity {
         Email = (TextView) findViewById(R.id.Email);
         Logout = (TextView) findViewById(R.id.Logout);
         changeProfile = (TextView) findViewById(R.id.changeProfile);
-//        welcome = (TextView) findViewById(R.id.welcome);
+        images = (ImageView) findViewById(R.id.images);
 
 
     }
@@ -54,6 +66,8 @@ public class Profile extends AppCompatActivity {
         } else {
 
             Email.setText(user.getEmail());
+            Fname.setText("Mật khẩu: Đã ẩn vì lý do bảo mật");
+
 
 //            progressBar.setVisibility(View.VISIBLE);
 //            showProfile(user);
@@ -76,10 +90,36 @@ public class Profile extends AppCompatActivity {
             }
         });
 
+        DatabaseReference profileRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid()).child("avatarUrl");
+        profileRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String avatarUrl = dataSnapshot.getValue(String.class);
+                if (!isActivityDestroyed) {
+                    if (avatarUrl != null) {
+                        // Hiển thị hình ảnh đại diện từ URL đã lưu trong Database
+                        Glide.with(Profile.this)
+                                .load(avatarUrl)
+                                .into(images);
+                    } else {
+                        // Hiển thị ảnh mặc định nếu không có URL trong Database
+                        images.setImageResource(R.drawable.images); // Thay thế R.drawable.images bằng ảnh mặc định của bạn
+                    }
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Xử lý lỗi nếu cần
+            }
+        });
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isActivityDestroyed = true;
 
-
+    }
 }
